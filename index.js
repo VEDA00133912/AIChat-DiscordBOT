7d3b4173d96bf122ce5ae5dd3911646f784a91ef
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-12-05 12:08:32 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const path = require('path');
+const fs = require('fs');
+require('dotenv').config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.commands = new Collection();
+
+const commandPath = path.join(__dirname, 'slashcommands');
+const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandPath, file);
+  const command = require(filePath);
+
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.error(`✗ ${filePath} に "data" または "execute" プロパティが不足しています`);
+  }
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+client.login(process.env.TOKEN);
